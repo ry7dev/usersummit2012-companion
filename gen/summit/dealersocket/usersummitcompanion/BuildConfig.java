9280da8a35bd87233b217e6c7e3b92a7c3a1e73e
ds-ryan <rluas@dealersocket.com>
2012-06-15 15:01:55 -0700

    refactoring...

--- gen/summit/dealersocket/usersummitcompanion/BuildConfig.java
+++ gen/summit/dealersocket/usersummitcompanion/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package summit.dealersocket.hellocordova;
+package summit.dealersocket.usersummitcompanion;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
